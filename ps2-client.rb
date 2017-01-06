3dc20f5bc9c0ecb5e01c7cad3fe514aa3d3f5c51
Jessica Stokes <hello@jessicastokes.net>
2017-01-05 22:25:47 -0800

    Add ps2-client

---
+++ ps2-client.rb
@@ -0,0 +1,10 @@
+class Ps2Client < Formula
+  desc "CLI tools for communicating with uLaunchElf on PlayStation 2"
+  homepage "https://github.com/ps2dev/ps2client"
+  head "https://github.com/ps2dev/ps2client.git"
+
+  def install
+    system "make"
+    bin.install "bin/fsclient", "bin/ps2client"
+  end
+end
