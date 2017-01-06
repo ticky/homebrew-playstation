91abe841d4d9a32cd6a32fc1585034cc2e28117f
Misty De Meo <mistydemeo@gmail.com>
2017-01-05 22:45:19 -0800

    pfs-shell: workaround for dylib paths

--- pfs-shell.rb
+++ pfs-shell.rb
@@ -1,3 +1,5 @@
+require "os/mac/mach"
+
 class PfsShell < Formula
   desc "CLI tool for browsing PlayStation 2 file system disks and images"
   homepage "https://github.com/uyjulian/pfsshell"
@@ -5,6 +7,12 @@ class PfsShell < Formula
 
   def install
     system "make"
+    %W[libapa.dylib libfakeps2sdk.dylib libiomanX.dylib libpfs.dylib].each do |dylib|
+      # Fix install names - the binary uses basenames without paths
+      MachO::Tools.change_install_name("pfsshell", dylib, "#{lib}/#{dylib}")
+      # File.realpath because these filenames are actually symlinks
+      lib.install File.realpath(dylib)
+    end
     bin.install "pfsshell"
   end
 end
