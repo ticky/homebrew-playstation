1bfd0912c3957ea4c4bb413120a75813558f06c7
Jessica Stokes <hello@jessicastokes.net>
2017-01-06 19:10:48 -0800

    omnishock: v0.0.2

--- omnishock.rb
+++ omnishock.rb
@@ -1,7 +1,7 @@
 class Omnishock < Formula
   desc "🎮 ¿¿¿ owo what’s this ???"
   homepage "https://github.com/ticky/omnishock"
-  url "https://github.com/ticky/omnishock.git", :tag => "v0.0.1"
+  url "https://github.com/ticky/omnishock.git", :tag => "v0.0.2"
 
   depends_on "rust" => :build
 
