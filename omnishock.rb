8bf1b4f1f5dcb4faf52ae54c45c149585a70d131
Jessica Stokes <hello@jessicastokes.net>
2017-01-06 19:04:33 -0800

    Add omnishock

---
+++ omnishock.rb
@@ -0,0 +1,12 @@
+class Omnishock < Formula
+  desc "🎮 ¿¿¿ owo what’s this ???"
+  homepage "https://github.com/ticky/omnishock"
+  url "https://github.com/ticky/omnishock.git", :tag => "v0.0.1"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "build", "--release"
+    bin.install "target/release/omnishock"
+  end
+end
