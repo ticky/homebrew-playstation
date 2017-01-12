9c765a05a12bf2336650b70caaa80223b22cb76b
Jessica Stokes <hello@jessicastokes.net>
2017-01-11 22:53:53 -0800

    Upgrade pfs-shell to version which supports 64-bit

--- pfs-shell.rb
+++ pfs-shell.rb
@@ -1,18 +1,13 @@
-require "os/mac/mach"
-
 class PfsShell < Formula
   desc "CLI tool for browsing PlayStation 2 file system disks and images"
   homepage "https://github.com/uyjulian/pfsshell"
+  version "2017-01-08T20:00:43Z"
+  url "https://github.com/uyjulian/pfsshell.git", :revision => "7bd15910871a9f0c8a787a5540b38adfd9427993"
+
   head "https://github.com/uyjulian/pfsshell.git"
 
   def install
     system "make"
-    %W[libapa.dylib libfakeps2sdk.dylib libiomanX.dylib libpfs.dylib].each do |dylib|
-      # Fix install names - the binary uses basenames without paths
-      MachO::Tools.change_install_name("pfsshell", dylib, "#{lib}/#{dylib}")
-      # File.realpath because these filenames are actually symlinks
-      lib.install File.realpath(dylib)
-    end
     bin.install "pfsshell"
   end
 end
