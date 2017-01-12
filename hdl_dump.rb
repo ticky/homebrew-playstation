6f0f9ac6629032cff1f69f3c144f110c3d188d7f
Jessica Stokes <hello@jessicastokes.net>
2017-01-12 15:06:43 -0800

    Anchor version of hdl_dump

--- hdl_dump.rb
+++ hdl_dump.rb
@@ -1,6 +1,9 @@
 class HdlDump < Formula
   desc "CLI tool to interact with Open PS2 Loader over a network"
   homepage "https://bitbucket.org/AKuHAK/hdl-dump"
+  url "https://bitbucket.org/AKuHAK/hdl-dump", :using => :hg, :revision => "a1b7ebcd0059e3c11645af5c92b46a9a934851b7"
+  version "2016-03-05T18:31:08Z"
+
   head "https://bitbucket.org/AKuHAK/hdl-dump", :using => :hg
 
   def install
