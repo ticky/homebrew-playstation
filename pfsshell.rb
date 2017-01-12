1cd83ca298c9da22d179461c5b83cd5199332afd
Jessica Stokes <hello@jessicastokes.net>
2017-01-12 15:07:17 -0800

    Fix some audit warnings in pfsshell

--- pfsshell.rb
+++ pfsshell.rb
@@ -1,8 +1,8 @@
-class PfsShell < Formula
+class Pfsshell < Formula
   desc "CLI tool for browsing PlayStation 2 file system disks and images"
   homepage "https://github.com/uyjulian/pfsshell"
-  version "2017-01-08T20:00:43Z"
   url "https://github.com/uyjulian/pfsshell.git", :revision => "7bd15910871a9f0c8a787a5540b38adfd9427993"
+  version "2017-01-08T20:00:43Z"
 
   head "https://github.com/uyjulian/pfsshell.git"
 
