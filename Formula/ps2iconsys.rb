31645b88c8999029aaee1c11d6fda503da1d8ee5
Jessica Stokes <hello@jessicastokes.net>
2017-01-25 16:52:12 -0800

    Add ps2iconsys v1.0

---
+++ Formula/ps2iconsys.rb
@@ -0,0 +1,17 @@
+class Ps2iconsys < Formula
+  desc "PS2 IconSys Tools"
+  homepage "http://www.ghulbus-inc.de/projects/ps2iconsys/index.html"
+  url "https://github.com/ticky/ps2iconsys.git", :revision => "0d9348d6174c729c0a3a4d80954bec5cc4690e55"
+  version "1.0"
+
+  head "https://github.com/ticky/ps2iconsys.git"
+
+  def install
+    system "make"
+    bin.install "obj_to_ps2icon", "ps2icon_to_obj", "iconsys_builder"
+  end
+
+  test do
+    system bin/"ps2icon_to_obj", "-h"
+  end
+end
