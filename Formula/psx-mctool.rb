f4fd241d7419b39a0d2f81fd213397c0749d3631
Jessica Stokes <hello@jessicastokes.net>
2017-02-17 22:58:07 +1100

    Swap version around b/c brew tests and such

--- Formula/psx-mctool.rb
+++ Formula/psx-mctool.rb
@@ -1,8 +1,8 @@
 class PsxMctool < Formula
   desc "Command line tool to manipulate PlayStation memory card files"
   homepage "https://github.com/simias/psx-mctool"
-  version "2016-10-16T16:24:20Z"
   url "https://github.com/simias/psx-mctool.git", :revision => "d9087b290e817363ce63fae1232ca282efb3e6c7"
+  version "2016-10-16T16:24:20Z"
 
   head "https://github.com/simias/psx-mctool.git"
 
