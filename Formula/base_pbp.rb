82eab739121a1a1652af24104bc91917f01146d7
Misty De Meo <mistydemeo@gmail.com>
2017-02-26 16:37:36 +1100

    base_pbp: add manual download formula

---
+++ Formula/base_pbp.rb
@@ -0,0 +1,25 @@
+class NullDownloadStrategy < CurlDownloadStrategy
+  def fetch
+    if !File.exist? @tarball_path
+      odie "You must manually download BASE.PBP and place it in: #{@tarball_path}"
+    else
+      super
+    end
+  end
+end
+
+class BasePbp < Formula
+  desc "Base Popstation executable image"
+  homepage "https://dontlook.here"
+  url "BASE.PBP", :using => NullDownloadStrategy
+  version "1.0"
+  sha256 "e0d49b137102eea2d50a9cfc8359e9cd835d10c3f657b59fc0f086b4671cec3d"
+
+  def install
+    pkgshare.install "BASE.PBP"
+  end
+
+  test do
+    File.exist? "#{pkgshare}/BASE.PBP"
+  end
+end
