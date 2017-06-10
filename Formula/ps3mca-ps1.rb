88ee59ebfad38600dedbc48ec27f9e759f4086bc
Jessica Stokes <hello@jessicastokes.net>
2017-06-09 19:32:43 -0700

    ps3mca-ps1: update to new revision with makefile!

--- Formula/ps3mca-ps1.rb
+++ Formula/ps3mca-ps1.rb
@@ -1,17 +1,16 @@
 class Ps3mcaPs1 < Formula
   desc "A program to read and write PS1 memory card with the Memory Card Adaptor"
   homepage "https://github.com/ticky/ps3mca-ps1"
-  url "https://github.com/ticky/ps3mca-ps1.git", :revision => "10646745ab2c12c8c20595f77f371b8ec32872a1"
-  version "2017-01-11T15:31:31Z"
+  url "https://github.com/ticky/ps3mca-ps1.git", :revision => "7f4dfc5a3c0502e03d97f9dc4ee6e227f71a1d2a"
+  version "2017-06-10T02:28:45Z"
 
   head "https://github.com/ticky/ps3mca-ps1.git"
 
+  depends_on "pkg-config" => :build
   depends_on "libusb"
 
   def install
-    cd "src" do
-      system ENV.cc, "-g", "-Wall", "-lusb-1.0", "-o", "ps3mca-ps1", "main.c"
-      bin.install "ps3mca-ps1"
-    end
+    system "make"
+    bin.install "ps3mca-ps1"
   end
 end
