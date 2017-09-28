2bb404e350eb5e2f7a4ecae41bdd3228cd769104
Jessica Stokes <hello@jessicastokes.net>
2017-09-27 20:40:18 -0700

    pfsshell: bump to new July 2017 revision

--- Formula/pfsshell.rb
+++ Formula/pfsshell.rb
@@ -1,8 +1,8 @@
 class Pfsshell < Formula
   desc "CLI tool for browsing PlayStation 2 file system disks and images"
   homepage "https://github.com/uyjulian/pfsshell"
-  url "https://github.com/uyjulian/pfsshell.git", :revision => "7bd15910871a9f0c8a787a5540b38adfd9427993"
-  version "2017-01-08T20:00:43Z"
+  url "https://github.com/uyjulian/pfsshell.git", :revision => "2b9d03bb78a67d812842f8456f475978e4328d08"
+  version "2017-07-23T18:58:41Z"
 
   head "https://github.com/uyjulian/pfsshell.git"
 
