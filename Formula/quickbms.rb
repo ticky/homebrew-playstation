335200972a948907e4dd66363c8c3d3f938b597a
Misty De Meo <mistydemeo@gmail.com>
2019-02-11 21:27:47 -0800

    quickbms: patch in missing header

--- Formula/quickbms.rb
+++ Formula/quickbms.rb
@@ -11,10 +11,20 @@ class Quickbms < Formula
   # Crashes in `brew test`; not sure why yet
   fails_with :gcc => "8"
 
+  # Adds back a missing header which is used under certain circumstances.
+  patch do
+    url "https://gist.githubusercontent.com/mistydemeo/b0f0067a560bb868e80ef6aee21874ae/raw/026e695c28d00becb87de66695793d0db31a37b1/quickbms_citycrc.patch"
+    sha256 "305d60e4b86734be3bacd5ccc584ed239d4affe4b5a239a43763703d493bb76a"
+  end
+
   def install
     # The makefile calls brew, which fails when run from *inside* brew because
     # brew isn't in the path. It's brewception!
     inreplace "Makefile", "`brew --prefix openssl`", Formula["openssl"].opt_prefix
+
+    # Fix a couple of includes to support non-system paths
+    inreplace "encryption/city.cc", "<citycrc.h>", '"citycrc.h"'
+    inreplace "encryption/citycrc.h", "<city.h>", '"city.h"'
 
     system "make"
     system "make", "install", "PREFIX=#{prefix}"
