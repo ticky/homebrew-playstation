24255f452e367ca4e76b6053656e3fc0395b626c
Jessica Stokes <hello@jessicastokes.net>
2019-05-20 21:20:46 -0700

    Update PALadin to v1.3

--- Formula/paladin.rb
+++ Formula/paladin.rb
@@ -1,8 +1,7 @@
 class Paladin < Formula
   desc "Enhanced PAL to NTSC Converter for True PSX NTSC Conversion"
   homepage "https://github.com/ticky/paladin"
-  url "https://github.com/ticky/paladin.git", :revision => "104bb50eaff769b089916107c12df99b2d1fcb93"
-  version "1.2"
+  url "https://github.com/ticky/paladin.git", :tag => "v1.3"
 
   head "https://github.com/ticky/paladin.git"
 
