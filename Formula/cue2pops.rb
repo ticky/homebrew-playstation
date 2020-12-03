d9f27aa70fc4e8f58682712edbb51ee8fb50aabf
Jessica Stokes <282113+ticky@users.noreply.github.com>
2020-12-03 04:53:41 +0000

    cue2pops: add 2015-11-04T502728Z_1 bottle.

--- Formula/cue2pops.rb
+++ Formula/cue2pops.rb
@@ -7,6 +7,12 @@ class Cue2pops < Formula
 
   head "https://github.com/ErikAndren/cue2pops-mac.git"
 
+  bottle do
+    root_url "https://github.com/ticky/homebrew-playstation/releases/download/cue2pops-2015-11-04T502728Z_1"
+    cellar :any_skip_relocation
+    sha256 "7045d3efd0cd76d66d0fb164577fef31d19674da144ac6d3460ea5f323c7d5ba" => :catalina
+  end
+
   depends_on :macos
 
   def install
