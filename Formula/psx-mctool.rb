db3d0ab8527a475bcf66c1c1bf482cd5cb6af2df
Jessica Stokes <282113+ticky@users.noreply.github.com>
2020-12-03 05:00:30 +0000

    psx-mctool: add 2016-10-16T462420Z_1 bottle.

--- Formula/psx-mctool.rb
+++ Formula/psx-mctool.rb
@@ -7,6 +7,13 @@ class PsxMctool < Formula
 
   head "https://github.com/simias/psx-mctool.git"
 
+  bottle do
+    root_url "https://github.com/ticky/homebrew-playstation/releases/download/psx-mctool-2016-10-16T462420Z_1"
+    cellar :any_skip_relocation
+    sha256 "01d0cff524961c6a5a3e1a2f44ee888410e10709791c1c3c4fe60283715778d0" => :catalina
+    sha256 "90d4a65237bc6bcf0807a650f7da3c1b9ef913bf6ce2d93b7cec3649212767aa" => :x86_64_linux
+  end
+
   depends_on "rust" => :build
 
   def install
