4238ce316ad1087cb00771fd86380112fe384eea
Jessica Stokes <282113+ticky@users.noreply.github.com>
2020-12-03 04:04:00 +0000

    ps3mca-ps1: add 2020-03-29T111341Z bottle.

--- Formula/ps3mca-ps1.rb
+++ Formula/ps3mca-ps1.rb
@@ -6,6 +6,12 @@ class Ps3mcaPs1 < Formula
 
   head "https://github.com/paolo-caroni/ps3mca-ps1.git"
 
+  bottle do
+    root_url "https://github.com/ticky/homebrew-playstation/releases/download/ps3mca-ps1-2020-03-29T111341Z"
+    cellar :any
+    sha256 "76a330055ae948bfb6080a866b2950965b5f70ac1ff009a9ccee31d9108466c0" => :catalina
+  end
+
   depends_on "pkg-config" => :build
   depends_on "libusb-compat"
   depends_on :macos
