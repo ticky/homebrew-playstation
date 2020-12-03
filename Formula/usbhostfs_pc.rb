7c1e00f288a2f2b7814f4ba9187fd3a6285aec16
Jessica Stokes <282113+ticky@users.noreply.github.com>
2020-12-03 03:56:33 +0000

    usbhostfs_pc: add 2020-06-24T131548Z bottle.

--- Formula/usbhostfs_pc.rb
+++ Formula/usbhostfs_pc.rb
@@ -6,6 +6,13 @@ class UsbhostfsPc < Formula
 
   head "https://github.com/pspdev/psplinkusb.git"
 
+  bottle do
+    root_url "https://github.com/ticky/homebrew-playstation/releases/download/usbhostfs_pc-2020-06-24T131548Z"
+    cellar :any
+    sha256 "b15e8197344179193ced7fc3c14438aab886be45ffcd868fdcb30b9e6af76489" => :catalina
+    sha256 "f562e4a2006f1e3ffe9c1033276b70663147319c9860ccfe78b5b11bb6e1eeef" => :x86_64_linux
+  end
+
   depends_on "libusb-compat"
 
   def install
