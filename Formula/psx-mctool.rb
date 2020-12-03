74e45e6e9a28c28ae1cf66fe805da835b58da359
Jessica Stokes <hello@jessicastokes.net>
2020-12-02 20:47:33 -0800

    Bump revision on PSX-mctool
    Closes #41.
    
    Signed-off-by: Jessica Stokes <282113+ticky@users.noreply.github.com>

--- Formula/psx-mctool.rb
+++ Formula/psx-mctool.rb
@@ -3,6 +3,7 @@ class PsxMctool < Formula
   homepage "https://github.com/simias/psx-mctool"
   url "https://github.com/simias/psx-mctool.git", revision: "d9087b290e817363ce63fae1232ca282efb3e6c7"
   version "2016-10-16T462420Z"
+  revision 1
 
   head "https://github.com/simias/psx-mctool.git"
 
