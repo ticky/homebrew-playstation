6f7b3608443399d39710bf759af71555b7b989cf
Jessica Stokes <hello@jessicastokes.net>
2020-12-02 20:33:33 -0800

    Bump revision on PALAdin
    Closes #37.
    
    Signed-off-by: Jessica Stokes <282113+ticky@users.noreply.github.com>

--- Formula/paladin.rb
+++ Formula/paladin.rb
@@ -2,6 +2,7 @@ class Paladin < Formula
   desc "Enhanced PAL to NTSC Converter for True PSX NTSC Conversion"
   homepage "https://github.com/ticky/paladin"
   url "https://github.com/ticky/paladin.git", tag: "v1.3"
+  revision 1
 
   head "https://github.com/ticky/paladin.git"
 
