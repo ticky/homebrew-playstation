06c2eafb645136ed0b7eb4a8791a76ab32118f01
Jessica Stokes <282113+ticky@users.noreply.github.com>
2020-12-03 04:45:51 +0000

    pfsshell: add 1.1.1_1 bottle.

--- Formula/pfsshell.rb
+++ Formula/pfsshell.rb
@@ -6,6 +6,13 @@ class Pfsshell < Formula
 
   head "https://github.com/ps2homebrew/pfsshell.git"
 
+  bottle do
+    root_url "https://github.com/ticky/homebrew-playstation/releases/download/pfsshell-1.1.1_1"
+    cellar :any_skip_relocation
+    sha256 "d1cdf106bd21148a34b609a35e7df1d39214364e3055d2f6e16fd651bf9ee16b" => :catalina
+    sha256 "8eda785e237ae7618186219b15e3d5393a36e49aaa92d8d34e719936e2b735d9" => :x86_64_linux
+  end
+
   depends_on "meson" => :build
   depends_on "ninja" => :build
 
