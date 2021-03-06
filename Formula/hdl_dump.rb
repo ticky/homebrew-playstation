5039fe294c0a256ffc3e897a74da052869f2061f
Jessica Stokes <282113+ticky@users.noreply.github.com>
2021-03-06 06:02:43 +0000

    hdl_dump: update 2020-07-27T400351Z_2 bottle.

--- Formula/hdl_dump.rb
+++ Formula/hdl_dump.rb
@@ -8,9 +8,10 @@ class HdlDump < Formula
   head "https://github.com/AKuHAK/hdl-dump.git"
 
   bottle do
-    root_url "https://github.com/ticky/homebrew-playstation/releases/download/hdl_dump-2020-07-27T400351Z_1"
-    sha256 cellar: :any_skip_relocation, catalina:     "d7d97d9fc8390342bdd7d09561e6e9bcdf22474b9a4ff40da4e3863f2b6c1249"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "cacb765fcd0fd928b613fd276f9aac9c6457735461f9d6f51edc16f3bd283b68"
+    root_url "https://github.com/ticky/homebrew-playstation/releases/download/hdl_dump-2020-07-27T400351Z_2"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, catalina:     "9f270a3ff12b3d0d497282666b850f5cf54d465499cbcb572627610d1f9d887d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "9b9623f6147e686915746e7c347a30dfc8205047435a3bef843414a87067e1a1"
   end
 
   def install
