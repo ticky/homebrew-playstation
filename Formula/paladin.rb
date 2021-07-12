80983ca6f02940fae1c59b6095e4ac63975aa435
Jessica Stokes <282113+ticky@users.noreply.github.com>
2021-07-12 16:54:56 +0000

    paladin: update 1.3_1 bottle.

--- Formula/paladin.rb
+++ Formula/paladin.rb
@@ -13,9 +13,9 @@ class Paladin < Formula
 
   bottle do
     root_url "https://github.com/ticky/homebrew-playstation/releases/download/paladin-1.3_1"
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, catalina:     "923aab64188fd4aab7eb795c8c4611a6e1a301ebc533f17d7a7d4acbfa2dc1fc"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "5e56e955445fcc0657baa0dfd91b0a1c0676842680476f2f0d86bf88c7143eba"
+    rebuild 2
+    sha256 cellar: :any_skip_relocation, catalina:     "51e297564a0bbf39639b70d5f360215c2fc48fd41f2b61b8b41d0996430e0f8d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "ca4ee9788342647b172e1f1aa02277e934373756a07b54da9aad1ed392ed2ff5"
   end
 
   def install
