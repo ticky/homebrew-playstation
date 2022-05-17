176ce18215a95907aaf25c97410e2af6a1e0fb54
Jessica Stokes <282113+ticky@users.noreply.github.com>
2022-05-17 17:39:36 +0000

    cisoplus: add 2022-05-17T035542Z bottle.

--- Formula/cisoplus.rb
+++ Formula/cisoplus.rb
@@ -6,6 +6,12 @@ class Cisoplus < Formula
 
   head "https://github.com/ticky/cisoplus.git"
 
+  bottle do
+    root_url "https://github.com/ticky/homebrew-playstation/releases/download/cisoplus-2022-05-17T035542Z"
+    sha256 cellar: :any_skip_relocation, big_sur:      "61f3c4ee9a940178698c54c19abaa9ba32af18d8a9fb9346aa6e2eaa027ac327"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "6edb6ddd6088cb80eacccebdbab26fd44443a5f8a3d17f8ddba1c03d09c5eb20"
+  end
+
   def install
     bin.install "cisoplus.py" => "cisoplus"
   end
